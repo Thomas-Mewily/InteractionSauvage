fd404961aeb646c98270cca616311222e821e298
Thomas-Mewily <tstd36@yahoo.fr>
2023-11-03 16:42:18 +0100

    test

--- InteractionSauvage/Interruptions/All.cs
+++ InteractionSauvage/Interruptions/All.cs
@@ -1,4 +1,5 @@
 ﻿using InteractionSauvage.MachineEtats;
+using Useful;
 using static InteractionSauvage.MachineEtats.Etat;
 
 namespace InteractionSauvage.Interruptions;
@@ -22,3 +23,64 @@ public class Apres : Interruption
     }
     public override bool Interrupt => Simu.Time - AssocieA.TempsDebut > Temps;
 }
+
+public class ApresAleatoire : Interruption
+{
+    public Temps Temps;
+    public ApresAleatoire(Temps min, Temps max)
+    {
+        Temps = Rand.IntUniform(min.T, max.T);
+    }
+    /*
+    public override void Load(Entite e)
+    {
+        base.Load(e);
+    }*/
+    public override bool Interrupt => Simu.Time - AssocieA.TempsDebut > Temps;
+}
+
+public class NourritureAtteignable : Interruption
+{
+    public override bool Interrupt => E.Target != null && E.Target.Vivant ? E.DistanceTo(E.Target) < E.Taille*2 : false;
+}
+
+public class Fatigue : Interruption
+{
+    public int NiveauEnergie;
+    public Fatigue(int niveauEnergie = 0)
+    {
+        NiveauEnergie = niveauEnergie;
+    }
+    public override bool Interrupt => E.Energie < NiveauEnergie;
+}
+
+public class Repose : Interruption
+{
+    public int NiveauEnergie;
+    public Repose(int niveauEnergie)
+    {
+        NiveauEnergie = niveauEnergie;
+    }
+    public override bool Interrupt => E.Energie >= NiveauEnergie;
+}
+
+public class Faim : Interruption
+{
+    public int NiveauNouriture;
+    public Faim(int niveauNouriture = 0)
+    {
+        NiveauNouriture = niveauNouriture;
+    }
+    public override bool Interrupt => E.Nourriture < NiveauNouriture;
+}
+
+public class Repu : Interruption
+{
+    public int NiveauNouriture;
+    public Repu(int niveauNouriture)
+    {
+        NiveauNouriture = niveauNouriture;
+    }
+    public override bool Interrupt => E.Nourriture >= NiveauNouriture;
+}
+
